53eabe6ad43efc61c16d4fbaa5c2312c388bbe02
Wade Burelbach <3250671+fafnirical@users.noreply.github.com>
2024-10-25 22:24:40 -0700

    Add Prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('prettier').Config}
+ */
+const config = {
+  experimentalTernaries: true,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
